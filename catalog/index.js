d9d0defca8a69a095f34d6bc9253fbbf48a1fb70
Anna Wiederkehr <wiederkehr.ac@gmail.com>
2018-06-25 20:22:24 +0200

    Change theme

--- catalog/index.js
+++ catalog/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Catalog, pageLoader } from "catalog";
 
+const theme =
+  {
+    brandColor: "#00205B",
+    pageHeadingBackground: "#000000",
+    sidebarColorActive: "#505DCD",
+    sidebarColorText: "#00205B",
+    sidebarColorTextActive: "#505DCD",
+    sidebarColorHeading: "#00205B",
+    codeColor: "#00205B",
+    linkColor: "#505DCD",
+    textMedium: "#00205B",
+    bgDark: "#000000",
+    bgLight: "#FFFFFF"
+  };
+  
 const pages = [
   {
     path: "/",
